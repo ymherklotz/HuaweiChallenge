8fdf8ebc8be2b6ee9e9234db79202e452153d6fb
Martin Ferianc <ferianc.martin@gmail.com>
2017-10-13 18:33:04 +0100

    [Code: Attemp 1] First attempt at debluring using Mean

--- src/deblurring.py
+++ src/deblurring.py
@@ -1,12 +1,18 @@
 import cv2
 import numpy as np
 import os
-import glob
-
-if __name__ == '__main__':
-    main()
+from matplotlib import pyplot as plt
 
 
 def main():
-    image_list = glob.glob('data/4000unlabeledLP/*.jpg')
-    x = np.array([np.array(Image.open(fname)) for img in image_list])
+    for filename in os.listdir('../data/4000unlabeledLP'):
+        img = cv2.imread('../data/4000unlabeledLP/'+filename)
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+        dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+        plt.subplot(121),plt.imshow(img)
+        plt.subplot(122),plt.imshow(dst)
+        plt.show()
+        break
+
+if __name__ == '__main__':
+    main()
