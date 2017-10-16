f047736c9b981c0ff91307a5a50f3ee8e838ddfd
Martin Ferianc <ferianc.martin@gmail.com>
2017-10-16 14:02:11 +0100

    [Code] Changing the neural net into a class so that it can be imported

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import argparse
 import cv2
+from network.py import NeuralNet
 
 #This function is adding padding to the original image
-#Stage 1  
+#Stage 1
 def add_padding(img):
     BLACK = [0,0,0]
     img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
@@ -20,11 +21,21 @@ def main():
     parser = argparse.ArgumentParser(description='Deblur image')
     parser.add_argument('--data', help='Input data image')
     parser.add_argument('--output', help='Output image')
+    #Stage 0 load the image
     args = parser.parse_args()
     input_name = args[0]
     destination_name = args[1]
     img = cv2.imread(input_name)
+    #Stage 1 add padding
     img = add_padding(img)
+
+    #Stage 2 openCV optimization
+
+    #Stage 3 Machine Learning Deblur
+    model = NeutalNet()
+    img = model.predict(img)
+
+    #Stage 4 More openCV
 
 
 
